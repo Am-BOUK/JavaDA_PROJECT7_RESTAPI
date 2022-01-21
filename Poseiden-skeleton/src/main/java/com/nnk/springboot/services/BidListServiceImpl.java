47572f5ea8037f679f60c5687417c8e3b2cbe096
Amal <ab.amal.boukili@gmail.com>
2022-01-21 15:07:39 +0100

    updat BidListService

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/services/BidListServiceImpl.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/services/BidListServiceImpl.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nnk.springboot.domain.BidList;
-import com.nnk.springboot.domain.User;
 import com.nnk.springboot.exception.EntityAlreadyExistException;
 import com.nnk.springboot.exception.EntityNotFoundException;
 import com.nnk.springboot.repositories.BidListRepository;
