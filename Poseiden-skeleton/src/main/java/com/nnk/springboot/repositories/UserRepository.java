27f520e6bef6c195ff12bfb6c9a8fbd534701d98
Amal <ab.amal.boukili@gmail.com>
2022-01-21 14:48:23 +0100

    add findByUserName in userRepository

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/repositories/UserRepository.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/repositories/UserRepository.java
@@ -1,6 +1,9 @@
 package com.nnk.springboot.repositories;
 
 import com.nnk.springboot.domain.User;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
@@ -9,5 +12,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer>, JpaSpecificationExecutor<User> {
+	public Optional<User> findByUsername(String username);
 
 }
